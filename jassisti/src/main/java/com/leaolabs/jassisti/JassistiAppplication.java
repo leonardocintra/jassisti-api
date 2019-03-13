1169c3d176cd094ecf3e24e6949f84294d808574
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-03-13 12:31:49 -0300

    Initial Project

---
+++ jassisti/src/main/java/com/leaolabs/jassisti/JassistiAppplication.java
@@ -0,0 +1,23 @@
+package com.leaolabs.jassisti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class JassistiAppplication {
+    
+
+    @RequestMapping("/health")
+    @ResponseBody
+    String home() {
+        return "API IS OK!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JassistiAppplication.class, args);
+    }
+}
