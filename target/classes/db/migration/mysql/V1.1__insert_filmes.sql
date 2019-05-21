80fe52df4bf78c5b8a51f3cf086efc2f4eba2cc0
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-05-21 15:00:21 -0300

    remove insert filme

--- target/classes/db/migration/mysql/V1.1__insert_filmes.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO jassisti.filme (id, titulo, the_movie_db_id, sinopse, imagem_poster, data_lancamento) VALUES(1, 'Titanic', 111222, '''teste ronaldin''', 'teste sem imagem', '2001-11-01 22:00:00.000');
-INSERT INTO jassisti.filme (id, titulo, the_movie_db_id, sinopse, imagem_poster, data_lancamento) VALUES(2, 'Rei Leão', 222111, '''teste ronaldin''', 'teste sem imagem', '2001-11-01 22:00:00.000');
-INSERT INTO jassisti.filme (id, titulo, the_movie_db_id, sinopse, imagem_poster, data_lancamento) VALUES(3, 'Guerra dos Mundos', 333222, '''teste ronaldin''', 'teste sem imagem', '2001-11-01 22:00:00.000');
-INSERT INTO jassisti.filme (id, titulo, the_movie_db_id, sinopse, imagem_poster, data_lancamento) VALUES(5, 'Rambo V', 333444, '''teste ronaldin''', 'teste sem imagem', '2001-11-01 22:00:00.000');
-
-
-
